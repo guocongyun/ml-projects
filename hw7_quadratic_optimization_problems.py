90ce272ecd7f86df86a10773b5bee1ce42462101
y49112cg@manchester.ac.uk <y49112cg@manchester.ac.uk>
2020-07-16 07:44:14 +0100

    Learnt to plot contour graphs

---
+++ hw7_quadratic_optimization_problems.py
@@ -0,0 +1,64 @@
+import cvxopt 
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f_original(x):
+    return 3*x[:,0]**2 + x[:,1]**2 + 2*x[:,0]*x[:,1] + x[:,0] + 6*x[:,1] + 2
+
+def f_matrix(x):
+    P = np.array([[1.5, 1],
+                  [1, 0.5]])
+    q = np.array([[1], [6]])
+    # IMPORTANT numpy array.T is the transpose form
+    return 1/2 * x.T @ P @ x + q.T @ x
+
+def constraints_satisfied(x):
+    G = np.array([
+        [-2, 3],
+        [-1, 0],
+        [0, -1],
+    ])
+    h = np.array([[4], [0], [0]])
+    return G @ x <= h
+
+
+
+xx, yy = np.linspace(-2, 20,100), np.linspace(-5, 5,100) # IMPORTANT, xx, yy === x1, x2
+XX, YY = np.meshgrid(xx, yy) # IMPORTANT, XX = [[-2...20]..], YY = [[-5...-5]...[5...5]]
+print(XX) 
+print(YY)
+ZZ_matrix = np.zeros(XX.shape)
+ZZ_original = np.zeros(XX.shape)
+constraints = np.zeros(XX.shape)
+
+for iy in range(XX.shape[0]): # IMPORTANT, np array.shape[0] ==  len(array), nparray.shape[1] == len(array[0])
+    for ix in range(XX.shape[1]):
+        x = np.array([[XX[iy,ix]], [YY[iy,ix]]])
+        ZZ_matrix[iy,ix] = f_matrix(x)
+        if np.all(constraints_satisfied(x)):
+            constraints[iy,ix] = 1
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.contourf(XX, YY, ZZ_matrix, 100)
+plt.contour(XX, YY, constraints)
+
+plt.show()
+
+P = cvxopt.matrix(np.array([[1.5, 1],
+              [1.0, 0.5]]))
+
+q = cvxopt.matrix(np.array([[1.0], 
+              [6.0]]))
+
+G = cvxopt.matrix(np.array([
+    [-2.0, 3.0],
+    [-1.0, 0.0],
+    [0.0, -1.0],
+]))
+    
+h = cvxopt.matrix(np.array([[4.0], 
+              [0.0], 
+              [0.0]]))
+
+print(cvxopt.solvers.qp(P, q, G, h)["x"])
