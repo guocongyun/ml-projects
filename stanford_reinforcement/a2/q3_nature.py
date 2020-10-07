6d32c14c3b09200b8fda0eeb0233a0cd0da18d07
guocongyun <congyun.guo@student.manchester.ac.uk>
2020-10-07 22:06:08 +0200

    finished assignment 2

---
+++ stanford_reinforcement/a2/q3_nature.py
@@ -0,0 +1,95 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+# IMPORTANT the below code prevent error with tf.Conv2D
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+from utils.general import get_logger
+from utils.test_env import EnvTest
+from q1_schedule import LinearExploration, LinearSchedule
+from q2_linear import Linear
+
+
+from configs.q3_nature import config
+
+
+class NatureQN(Linear):
+    """
+    Implementing DeepMind's Nature paper. Here are the relevant urls.
+    https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
+    https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf
+    """
+    def get_q_values_op(self, state, scope, reuse=False):
+        """
+        Returns Q values for all actions
+
+        Args:
+            state: (tf tensor) 
+                shape = (batch_size, img height, img width, nchannels)
+            scope: (string) scope name, that specifies if target network or not
+            reuse: (bool) reuse of variables in the scope
+
+        Returns:
+            out: (tf tensor) of shape = (batch_size, num_actions)
+        """
+        # this information might be useful
+        num_actions = self.env.action_space.n
+
+        ##############################################################
+        """
+        TODO: implement the computation of Q values like in the paper
+                https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
+                https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf
+
+              you may find the section "model architecture" of the appendix of the 
+              nature paper particulary useful.
+
+              store your result in out of shape = (batch_size, num_actions)
+
+        HINT: 
+            - You may find the following functions useful:
+                - tf.layers.conv2d
+                - tf.layers.flatten
+                - tf.layers.dense
+
+            - Make sure to also specify the scope and reuse
+
+        """
+        ##############################################################
+        ################ YOUR CODE HERE - 10-15 lines ################ 
+
+        with tf.compat.v1.variable_scope(scope):
+            hidden1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(8, 8), padding='same', strides=(4, 4), activation=tf.nn.relu).apply(state)
+            hidden2 = tf.keras.layers.Conv2D(filters=64, kernel_size=(4, 4), padding='same', strides=(2, 2), activation=tf.nn.relu).apply(hidden1)
+            hidden3 = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', strides=(1, 1), activation=tf.nn.relu).apply(hidden2)
+            flat = tf.keras.layers.Flatten().apply(hidden3)
+            hidden4 = tf.keras.layers.Dense(512, activation=tf.nn.relu).apply(flat)
+            out = tf.keras.layers.Dense(num_actions).apply(hidden4)
+
+        ##############################################################
+        ######################## END YOUR CODE #######################
+        return out
+
+
+"""
+Use deep Q network for test environment.
+"""
+if __name__ == '__main__':
+    env = EnvTest((80, 80, 1))
+
+    # exploration strategy
+    exp_schedule = LinearExploration(env, config.eps_begin, 
+            config.eps_end, config.eps_nsteps)
+
+    # learning rate schedule
+    lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
+            config.lr_nsteps)
+
+    # train model
+    model = NatureQN(env, config)
+    model.run(exp_schedule, lr_schedule)
